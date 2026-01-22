cdb86ec5f262da8f41ac8188f503b69c97f58567
Vijayalakshmi1823 <dvijayalakshmi911@gmail.com>
2026-01-22 12:51:32 +0530

    Added new doctypes-storycast, story_child

---
+++ kathe_khajane/kathe_khajane/doctype/storycast/storycast.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, kkadmin and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class Storycast(Document):
+	pass
